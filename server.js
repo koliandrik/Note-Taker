91d020f73bc3ab8336e8157ad970741282a90018
koliandrik <koliandrik@gmail.com>
2024-06-26 01:55:35 -0700

    added script

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
 // Start the server
 app.listen(PORT, () => 
     console.log(`Server is running on port ${PORT}`)
