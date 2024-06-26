8bfb3a072a4860c147f9c7c53a7a04a5e1f42ca6
koliandrik <koliandrik@gmail.com>
2024-06-26 02:37:23 -0700

    Add UUID generation for note IDs and define requests for note creation and retrieval

--- routes/notes.js
+++ routes/notes.js
@@ -1,25 +1,59 @@
 const notes = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
 
 // Define routes for notes
 notes.get('/', (req, res) => {
+    console.info(`${req.method} request received for notes`);
     // Handle GET request for all notes
-});
-
-notes.get('/:id', (req, res) => {
-    // Handle GET request for a specific note
+    readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
 });
 
 notes.post('/', (req, res) => {
+    console.info(`${req.method} request received to submit a note`);
     // Handle POST request to create a new note
-});
+    const { title, text } = req.body;
 
-notes.put('/:id', (req, res) => {
-    // Handle PUT request to update a specific note
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        };
+
+        readAndAppend(newNote, './db/notes.json');
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+        
+        res.json(response);
+    } else {
+        res.error('Error in adding note');
+    }
 });
 
 notes.delete('/:id', (req, res) => {
+    console.info(`${req.method} request received to delete a note`);
     // Handle DELETE request to delete a specific note
+    const noteId = req.params.id;
+    readFromFile('./db/notes.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            // Make a new array of all notes except the one with the ID provided in the URL
+            const result = json.filter((note) => note.note_id !== noteId);
+
+            // Save that array to the filesystem
+            readAndAppend(result, './db/notes.json');
+
+            const response = {
+                status: 'success',
+                body: result,
+            };
+
+            res.json(response);
+        });
 });
 
 module.exports = notes;
